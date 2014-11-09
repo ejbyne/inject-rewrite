bc48c0a5482b4c73fdb1fad79fac99063421c90c
ed <ejbyne@gmail.com>
2014-11-09 13:42:34 +0000

    First working recursion inject

--- lib/inject-rewrite.rb
+++ lib/inject-rewrite.rb
@@ -1,40 +1,34 @@
 class Array
 
-def inject_rewrite(sum = nil, &block)
-  array = self
-  if sum == nil
-    sum = array[0]
-    array.shift
-  end
-  array.each do |n|
-    sum = block.call(sum, n)
-  end
-  return sum
-end
-
-def inject_rewrite_recursion(sum = nil, &block)
-
-  array = self
-  sum = array[0]
-  n = array[0]
-
-  recursion = lambda do
-    if array.length > 0
-      sum = block.call(sum, n)
+  def inject_rewrite(sum = nil, &block)
+    array = self
+    if sum == nil
+      sum = array[0]
       array.shift
     end
+    array.each do |n|
+      sum = block.call(sum, n)
+    end
+    return sum
   end
 
-  sum = recursion.call(recursion.call(sum, n), n)
-
-  # sum = block.call(sum, inject_rewrite_recursion(sum, self[n]))
-
-
-  # # sum = block.call(sum, block.call(sum, self[n+1]))
-  # sum = block.call(sum, array[n]) + block.call(sum, array[n+1])
-  # return sum
-end
-
-
+  def inject_rewrite_recursion(sum = nil, &block)
+    array = self
+    if sum == nil
+      sum = array[0]
+      array.shift
+    end
+    n = array[0]
+    recursion_proc = Proc.new do #|array, sum, n|
+      sum = block.call(sum, n)
+      array.shift
+      if array.length == 0
+        return sum
+      else
+        recursion_proc.call #(array, sum, n)
+      end
+    end
+    recursion_proc.call #(array, sum, n)
+  end
 
 end
