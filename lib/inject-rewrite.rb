9a2ab007c8ff609e02eab0527c18c4d11f267ea0
ed <ejbyne@gmail.com>
2014-11-09 13:16:09 +0000

    first attempt at recursion

--- lib/inject-rewrite.rb
+++ lib/inject-rewrite.rb
@@ -12,4 +12,29 @@ def inject_rewrite(sum = nil, &block)
   return sum
 end
 
+def inject_rewrite_recursion(sum = nil, &block)
+
+  array = self
+  sum = array[0]
+  n = array[0]
+
+  recursion = lambda do
+    if array.length > 0
+      sum = block.call(sum, n)
+      array.shift
+    end
+  end
+
+  sum = recursion.call(recursion.call(sum, n), n)
+
+  # sum = block.call(sum, inject_rewrite_recursion(sum, self[n]))
+
+
+  # # sum = block.call(sum, block.call(sum, self[n+1]))
+  # sum = block.call(sum, array[n]) + block.call(sum, array[n+1])
+  # return sum
+end
+
+
+
 end
--- spec/inject-rewrite_spec.rb
+++ spec/inject-rewrite_spec.rb
@@ -2,29 +2,42 @@ require 'inject-rewrite'
 
 describe Array do
   
-  it "can aggregate the elements of [1, 1] to return 2" do
-    array = [1, 1]
-    expect(array.inject_rewrite { |sum, n| sum + n }).to eq(2)
+  context 'iteration' do
+  
+    it "can aggregate the elements of [1, 1] to return 2" do
+      array = [1, 1]
+      expect(array.inject_rewrite { |sum, n| sum + n }).to eq(2)
+    end
+
+    it "can aggregate the elements of [1, 2, 3, 4, 5] to return 15" do
+      array = [1, 2, 3, 4, 5]
+      expect(array.inject_rewrite { |sum, n| sum + n }).to eq(15)
+    end
+
+    it "can multiply the elements of [1, 2, 3, 4, 5] to return 120" do
+      array = [1, 2, 3, 4, 5]
+      expect(array.inject_rewrite { |sum, n| sum * n }).to eq(120)
+    end
+
+    it "can accept a starting number as an argument" do
+      array = [1, 2, 3, 4, 5]
+      expect(array.inject_rewrite(5) { |sum, n| sum + n }).to eq(20)
+    end
+
+    it "works with strings" do
+      array = ["Ed", "Sini", "Henry"]
+      expect(array.inject_rewrite { |sum, n| sum + n }).to eq("EdSiniHenry")
+    end
+
   end
 
-  it "can aggregate the elements of [1, 2, 3, 4, 5] to return 15" do
-    array = [1, 2, 3, 4, 5]
-    expect(array.inject_rewrite { |sum, n| sum + n }).to eq(15)
-  end
+  context 'recursion' do
 
-  it "can multiply the elements of [1, 2, 3, 4, 5] to return 120" do
-    array = [1, 2, 3, 4, 5]
-    expect(array.inject_rewrite { |sum, n| sum * n }).to eq(120)
-  end
+    it "can aggregate the elements of [1, 1] to return 2" do
+      array = [1, 1]
+      expect(array.inject_rewrite_recursion { |sum, n| sum + n }).to eq(2)
+    end
 
-  it "can accept a starting number as an argument" do
-    array = [1, 2, 3, 4, 5]
-    expect(array.inject_rewrite(5) { |sum, n| sum + n }).to eq(20)
-  end
-
-  it "works with strings" do
-    array = ["Ed", "Sini", "Henry"]
-    expect(array.inject_rewrite { |sum, n| sum + n }).to eq("EdSiniHenry")
   end
 
 end
