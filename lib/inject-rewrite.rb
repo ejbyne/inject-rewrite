2cccaed908925398bacc2df179525a26879dfc29
ed <ejbyne@gmail.com>
2014-11-09 14:18:41 +0000

    Minor refactoring

--- lib/inject-rewrite.rb
+++ lib/inject-rewrite.rb
@@ -1,18 +1,18 @@
 class Array
 
-  def inject_rewrite_iteration(sum = nil, &block)
+  def inject_rewrite_iteration(sum = nil, &inject_block)
     array = self
     if sum == nil
       sum = array[0]
       array.shift
     end
     array.each do |n|
-      sum = block.call(sum, n)
+      sum = inject_block.call(sum, n)
     end
     return sum
   end
 
-  def inject_rewrite_recursion(sum = nil, &block)
+  def inject_rewrite_recursion(sum = nil, &inject_block)
     array = self
     if sum == nil
       sum = array[0]
@@ -21,7 +21,7 @@ class Array
     n = array[0]
 
     recursion_proc = Proc.new do |array, sum, n|
-      sum = block.call(sum, n)
+      sum = inject_block.call(sum, n)
       array.shift
       n = array[0]
       if array.length == 0
