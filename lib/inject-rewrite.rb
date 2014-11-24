a067c89c3e7108912b30843b91c45945e079109e
ed <ejbyne@gmail.com>
2014-11-24 14:40:44 +0000

    refactored to allow for starting number and symbol

--- lib/inject-rewrite.rb
+++ lib/inject-rewrite.rb
@@ -1,26 +1,55 @@
 class Array
 
-  def inject_rewrite_iteration(sum = nil, &inject_block)
+  def inject_rewrite_iteration(first_option = nil, second_option = nil, &inject_block)
     array = self
-    if sum.is_a?(Symbol)
-      symbol_proc = sum.to_proc
+    if first_option == nil && second_option == nil
       sum = array[0]
       array.shift
-      array.each do |n|
-        sum = symbol_proc.call(sum, n)
-      end
-      return sum
-    else
-      if sum == nil
+    elsif first_option.is_a?(Symbol)
+      symbol_proc = first_option.to_proc
+      sum = array[0]
+      array.shift
+    elsif second_option.is_a?(Symbol)
+      symbol_proc = second_option.to_proc
+      if first_option == nil
         sum = array[0]
         array.shift
+      else
+        sum = first_option
       end
-      array.each do |n|
-        sum = yield(sum, n)
-      end
-    return sum
+    elsif first_option != nil
+      sum = first_option
     end
+    array.each do |n|
+      if block_given?
+        sum = yield(sum, n)
+      elsif symbol_proc
+        sum = symbol_proc.call(sum, n)
+      end
+    end
+    return sum
   end
+
+  #   array = self
+  #   if sum.is_a?(Symbol)
+  #     symbol_proc = sum.to_proc
+  #     sum = array[0]
+  #     array.shift
+  #     array.each do |n|
+  #       sum = symbol_proc.call(sum, n)
+  #     end
+  #     return sum
+  #   else
+  #     if sum == nil
+  #       sum = array[0]
+  #       array.shift
+  #     end
+  #     array.each do |n|
+  #       sum = yield(sum, n)
+  #     end
+  #   return sum
+  #   end
+  # end
 
   def inject_rewrite_recursion(sum = nil, &inject_block)
     array = self
@@ -31,7 +60,7 @@ class Array
     n = array[0]
 
     recursion_proc = Proc.new do |array, sum, n|
-      sum = inject_block.call(sum, n)
+      sum = yield(sum, n)
       array.shift
       n = array[0]
       if array.length == 0
--- spec/inject-rewrite_spec.rb
+++ spec/inject-rewrite_spec.rb
@@ -1,42 +1,54 @@
 require 'inject-rewrite'
 
 describe Array do
+
+  let (:array) { [1, 2, 3, 4, 5] }
   
   context 'iteration' do
   
     it "can aggregate the elements of [1, 1] to return 2" do
-      array = [1, 1]
-      expect(array.inject_rewrite_iteration { |sum, n| sum + n }).to eq(2)
+      short_array = [1, 1]
+      expect(short_array.inject_rewrite_iteration { |sum, n| sum + n }).to eq(2)
     end
 
     it "can aggregate the elements of [1, 2, 3, 4, 5] to return 15" do
-      array = [1, 2, 3, 4, 5]
       expect(array.inject_rewrite_iteration { |sum, n| sum + n }).to eq(15)
     end
 
     it "can multiply the elements of [1, 2, 3, 4, 5] to return 120" do
-      array = [1, 2, 3, 4, 5]
       expect(array.inject_rewrite_iteration { |sum, n| sum * n }).to eq(120)
     end
 
     it "can accept a starting number as an argument" do
-      array = [1, 2, 3, 4, 5]
       expect(array.inject_rewrite_iteration(5) { |sum, n| sum + n }).to eq(20)
     end
 
     it "works with strings" do
-      array = ["Ed", "Sini", "Henry"]
-      expect(array.inject_rewrite_iteration { |sum, n| sum + n }).to eq("EdSiniHenry")
+      string_array = ["Ed", "Sini", "Henry"]
+      expect(string_array.inject_rewrite_iteration { |sum, n| sum + n }).to eq("EdSiniHenry")
     end
 
-    it "can aggregate elements when passed the symbol(:+)" do
-      array = [1, 2, 3, 4, 5]
+    it "can aggregate elements when passed the symbol :+" do
       expect(array.inject_rewrite_iteration(:+)).to eq(15)
     end
 
-    it "can multiply elements when passed the symbol(:*)" do
-      array = [1, 2, 3, 4, 5]
+    it "can multiply elements when passed the symbol :*" do
       expect(array.inject_rewrite_iteration(:*)).to eq(120)
+    end
+
+    it "can take a starting number and the symbol :+" do
+      expect(array.inject_rewrite_iteration(5, :+)).to eq(20)
+    end
+
+    it "can take a starting number and the symbol :*" do
+      expect(array.inject_rewrite_iteration(5, :*)).to eq(600)
+    end
+
+    it "can find the longest word in an array" do
+      string_array = ["Ed", "Sini", "Henry"]
+      string_array.inject do |memo, word|
+        memo.length > word.length ? memo : word
+      end
     end
 
   end
@@ -44,22 +56,19 @@ describe Array do
   context 'recursion' do
 
     it "can aggregate the elements of [1, 1] to return 2" do
-      array = [1, 1]
-      expect(array.inject_rewrite_recursion { |sum, n| sum + n }).to eq(2)
+      short_array = [1, 1]
+      expect(short_array.inject_rewrite_recursion { |sum, n| sum + n }).to eq(2)
     end
 
     it "can aggregate the elements of [1, 2, 3, 4, 5] to return 15" do
-      array = [1, 2, 3, 4, 5]
       expect(array.inject_rewrite_recursion { |sum, n| sum + n }).to eq(15)
     end
 
      it "can multiply the elements of [1, 2, 3, 4, 5] to return 120" do
-      array = [1, 2, 3, 4, 5]
       expect(array.inject_rewrite_recursion { |sum, n| sum * n }).to eq(120)
     end
 
     it "can accept a starting number as an argument" do
-      array = [1, 2, 3, 4, 5]
       expect(array.inject_rewrite_recursion(5) { |sum, n| sum + n }).to eq(20)
     end
 
