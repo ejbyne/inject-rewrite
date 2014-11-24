a12b79082960c2b1a05897e682f9196df5e5cd86
ed <ejbyne@gmail.com>
2014-11-24 19:53:46 +0000

    further refactoring

--- lib/inject-rewrite.rb
+++ lib/inject-rewrite.rb
@@ -1,45 +1,39 @@
 class Array
 
-  def inject_rewrite_iteration(first_option = nil, second_option = nil, &block)
+  def inject_rewrite_iteration(*options, &block)
     array = self.dup
-    if first_option.is_a?(Symbol) || second_option.is_a?(Symbol)
-      symbol_iteration_method(array, first_option, second_option)
-    else
-      block_iteration_method(array, first_option, &block)
-    end
+    return symbol_iteration_method(array, options)if options.any? { |option| option.is_a?(Symbol) }
+    block_iteration_method(array, options[0], &block)
   end
 
-  def inject_rewrite_recursion(first_option = nil, second_option = nil, &block)
+  def inject_rewrite_recursion(*options, &block)
     array = self.dup
-    if first_option.is_a?(Symbol) || second_option.is_a?(Symbol)
-      symbol_recursion_method(array, first_option, second_option)
-    else
-      block_recursion_method(array, first_option, &block)
-    end
+    return symbol_recursion_method(array, options) if options.any? { |option| option.is_a?(Symbol) }
+    block_recursion_method(array, options[0], &block)
   end
 
-  def symbol_iteration_method(array, first_option, second_option)
-    symbol_proc = create_symbol_proc(first_option, second_option)
-    sum = select_symbol_initial_item(array, first_option, second_option)
+  def symbol_iteration_method(array, options)
+    symbol_proc = options.select{ |option| option.is_a?(Symbol) }.first.to_proc
+    sum = select_symbol_initial_item(array, options)
     array.each { |n| sum = symbol_proc.call(sum, n) }
     return sum
   end
 
-  def block_iteration_method(array, first_option, &block)
-    sum = select_block_initial_item(array, first_option)
+  def block_iteration_method(array, option, &block)
+    sum = select_block_initial_item(array, option)
     array.each { |n| sum = yield(sum, n) }
     return sum
   end
 
-  def symbol_recursion_method(array, first_option, second_option)
-    symbol_proc = create_symbol_proc(first_option, second_option)
-    sum = select_symbol_initial_item(array, first_option, second_option)
+  def symbol_recursion_method(array, options)
+    symbol_proc = options.select{ |option| option.is_a?(Symbol) }.first.to_proc
+    sum = select_symbol_initial_item(array, options)
     n = array[0]
     call_recursion(array, sum, n, &symbol_proc)
   end
 
-  def block_recursion_method(array, first_option, &block)
-    sum = select_block_initial_item(array, first_option)
+  def block_recursion_method(array, option, &block)
+    sum = select_block_initial_item(array, option)
     n = array[0]
     call_recursion(array, sum, n, &block)
   end
@@ -47,31 +41,20 @@ class Array
   def call_recursion(array, sum, n, &block)
     sum = block.call(sum, n)
     array.shift
-    n = array[0]
-    if array.length == 0
-      return sum
-    else
-      call_recursion(array, sum, n, &block)
-    end
+    return sum if array.empty?
+    call_recursion(array, sum, array[0], &block)
   end
 
-  def create_symbol_proc(first_option, second_option)
-    [first_option, second_option].select{ |option| option.is_a?(Symbol) }.first.to_proc
-  end
-
-  def select_symbol_initial_item(array, first_option, second_option)
-    if second_option.is_a?(Symbol)
-      sum = first_option
-    else
-      sum = array[0]
-      array.shift
-    end
+  def select_symbol_initial_item(array, options)
+    return options[0] if options[1].is_a?(Symbol)
+    sum = array[0]
+    array.shift
     return sum
   end
 
-  def select_block_initial_item(array, first_option)
-    unless first_option == nil
-      sum = first_option
+  def select_block_initial_item(array, option)
+    unless option == nil
+      sum = option
     else
       sum = array[0]
       array.shift
