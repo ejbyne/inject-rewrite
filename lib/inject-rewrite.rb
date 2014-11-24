e5a0f8f93a7c482ea102ff54f257acb915e2be41
ed <ejbyne@gmail.com>
2014-11-24 23:09:10 +0000

    symbol to block method and more refactoring

--- lib/inject-rewrite.rb
+++ lib/inject-rewrite.rb
@@ -2,32 +2,37 @@ class Array
 
   def inject_rewrite_iteration(*options, &block)
     array = self.dup
-    options.map { |option| block = option.to_proc if option.is_a?(Symbol) }
     sum = initial_sum(array, options)
-    array.shift if options[0].is_a?(Symbol) || options.empty?
-    array.each { |n| sum = block.call(sum, n) }
-    return sum
+    block = symbol_to_block(options) if options.any?{ |option| option.is_a?(Symbol) }
+    return iterative_method(array, sum, &block)
   end
 
   def inject_rewrite_recursion(*options, &block)
     array = self.dup
-    options.map { |option| block = option.to_proc if option.is_a?(Symbol) }
     sum = initial_sum(array, options)
-    array.shift if options[0].is_a?(Symbol) || options.empty?
-    n = array[0]
-    call_recursion(array, sum, n, &block)
+    block = symbol_to_block(options) if options.any?{ |option| option.is_a?(Symbol) }
+    return recursive_method(array, sum, array[0], &block)
   end
 
   def initial_sum(array, options)
-    return array[0] if options[0].is_a?(Symbol) || options.empty?
-    return options[0] if !options[0].is_a?(Symbol) && !options[0].nil?
+    return options[0] if !options[0].nil? && !options[0].is_a?(Symbol)
+    return array.shift if options[0].nil? || options[0].is_a?(Symbol)
   end
 
-  def call_recursion(array, sum, n, &block)
+  def symbol_to_block(options)
+    options.select{ |option| option.is_a?(Symbol) }.first.to_proc 
+  end
+
+  def iterative_method(array, sum, &block)
+    array.each { |n| sum = block.call(sum, n) }
+    return sum
+  end
+
+  def recursive_method(array, sum, n, &block)
     sum = block.call(sum, n)
     array.shift
     return sum if array.empty?
-    call_recursion(array, sum, array[0], &block)
+    recursive_method(array, sum, array[0], &block)
   end
 
 end
